9362bda764e73637d957b32ac0267d4a989c21d3
Stephen Montague <Stephen.D.Montague@gmail.com>
2023-06-20 18:14:58 -0400

    Learning about inline styles

--- App.jsx
+++ App.jsx
@@ -1,7 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-	return <View style={styles.container}></View>;
+	return (
+		<View style={styles.container}>
+			<View>
+				<Text>Another piece of text!</Text>
+			</View>
+			<Text
+				style={{
+					margin: 16,
+					borderWidth: 2,
+					borderColor: 'red',
+					padding: 16,
+				}}>
+				Hello World!
+			</Text>
+			<Button title='Tap me!' />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
