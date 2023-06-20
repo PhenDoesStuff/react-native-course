c6d964b6831219b0107442fa3c2e8a7cb4267051
Stephen Montague <Stephen.D.Montague@gmail.com>
2023-06-20 18:18:03 -0400

    Moving in line styling to the styles sheet object

--- App.jsx
+++ App.jsx
@@ -6,15 +6,7 @@ export default function App() {
 			<View>
 				<Text>Another piece of text!</Text>
 			</View>
-			<Text
-				style={{
-					margin: 16,
-					borderWidth: 2,
-					borderColor: 'red',
-					padding: 16,
-				}}>
-				Hello World!
-			</Text>
+			<Text style={styles.text}>Hello World!</Text>
 			<Button title='Tap me!' />
 		</View>
 	);
@@ -26,5 +18,11 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+	},
+	text: {
+		margin: 16,
+		borderWidth: 2,
+		padding: 16,
+		borderColor: 'red',
 	},
 });
