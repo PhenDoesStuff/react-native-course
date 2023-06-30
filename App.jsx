3fcfdb2a32ec03dca05d2b8440516ae7a27b9b2b
Stephen Montague <Stephen.D.Montague@gmail.com>
2023-06-29 22:20:07 -0400

    Using state to create a list of goals and render it to the screen

--- App.jsx
+++ App.jsx
@@ -1,17 +1,32 @@
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+	const [newGoal, setNewGoal] = useState('');
+	const [goals, setGoals] = useState([]);
+
+	const goalInputHandler = enteredText => {
+		setNewGoal(enteredText);
+	};
+
+	const addGoalHandler = () => {
+		setGoals(currentGoals => [...currentGoals, newGoal]);
+	};
+
 	return (
 		<View style={styles.appContainer}>
 			<View style={styles.inputContainer}>
 				<TextInput
 					style={styles.textInput}
 					placeholder='Your course goal!'
+					onChangeText={goalInputHandler}
 				/>
-				<Button title='Add Goal' />
+				<Button title='Add Goal' onPress={addGoalHandler} />
 			</View>
 			<View style={styles.goalsContainer}>
-				<Text>List of goals...</Text>
+				{goals.map((goal, idx) => (
+					<Text key={idx}>{goal}</Text>
+				))}
 			</View>
 		</View>
 	);
